77c3f17d29d7498106d485e568f506fc38a18654
noaw <noaw@bgu.ac.il>
2011-11-15 18:16:06 +0200

    added lab2

---
+++ lab2/canalyze.c
@@ -0,0 +1,67 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "namelist.h"
+//#include "keywords.h"
+#include <string.h>
+#include <assert.h>
+
+int main(int argc, char **argv) {
+  
+  
+	char *filename = NULL;
+	
+	FILE * file = NULL;
+	
+	char words[32][20] = {"auto", "double", "int", "long", "break", "else", "long", "switch", "case", "enum", "register", "typedef", "char", "extern", "return", "union", "const", "float", "short", "unsigned", "continue", "for", "signed", "oid", "default", "goto", "sizeof", "volatile", "do", "if", "static", "while"};	  
+	
+	int i;
+	int count=0;
+	char a[129];
+	char b[129];
+	int row = 1;
+	char word[20];
+	char *pointer = NULL;
+	int add = 1;
+	int fileCounter = 0;
+	namelist nl = make_namelist();
+	
+	for (fileCounter = 1; fileCounter<argc; fileCounter++){
+	  filename = argv[fileCounter];
+	  if ((file = fopen(filename, "r")) == NULL){
+	    printf("No file %d: %s \n",i,filename);
+	    return 1;
+	  }
+	  
+	  while (fgets(a, sizeof(a), file) != NULL){	  
+	    //printf ("Splitting string \"%s\" into tokens:\n",a);
+	    pointer = strtok (a," ,.-\(){}\n;*^%#@!?+_'\"<>[]");
+	    while (pointer != NULL)
+	    {
+	      for (i=0; i<sizeof(words); i++){
+		if (strcmp(pointer,words[i])==0){
+		  add = 0;
+		}
+	      }
+	      if (add==1 && isalpha(pointer[1])){
+		add_name(nl,pointer);
+	      }
+	      pointer = strtok (NULL," ,.-\(){}\n;*^%#@!?+_'\"<>[]");
+	      add = 1;
+	    }
+	    
+	  }
+	  fclose(file);
+	}
+	
+	char nl1[nl->size][20];
+	for(i = 0; i!=nl->size; ++i) {
+	  nl1[i] = nl[i];
+	}
+	printf("Here is the list: \n");
+	for(i = 0; i!=nl->size; ++i) {
+	  printf("%s %d\n",nl->names[i].name,nl->names[i].count);
+	}
+	
+	//printf("\n");
+	return 1;
+}

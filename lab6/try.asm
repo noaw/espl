73808f23cfff2509c5654ee671c8f0353282c3cb
noaw <noaw@bgu.ac.il>
2011-12-20 17:25:42 +0200

    lab6

---
+++ lab6/try.asm
@@ -0,0 +1,39 @@
+: MOV	DH, 'A'
+: MOV	DL, 'Z'
+: 
+: MOV	AH, DH	; X='A'
+: 
+: Loop1:
+: CMP	AH, DL	; X <= 'Z'
+: JA	End_Loops
+: 
+: MOV	AL, DH	; Y = 'A'
+: 
+: Loop2:
+: CMP	AL, AH	; Y <= X
+: JA	End_Loop1
+: 
+: PUSH	DX
+: PUSH	AX
+: 
+: ; print symbol in AL here
+: 
+: POP	AX
+: POP	DX
+: 
+: INC	AL
+: JMP	Loop2
+: 
+: End_Loop1:
+: PUSH	DX
+: PUSH	AX
+: 
+: ; print symbol '\n' here
+: 
+: POP	AX
+: POP	DX
+: 
+: INC	AH	; X++
+: JMP	Loop1
+: 
+: End_Loops:
